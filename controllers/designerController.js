85ba0b8a812326f2c5809095e3990394025dae8f
Sullivown <daniel.w.t.sullivan@gmail.com>
2022-10-20 14:56:53 +0100

    add all desinger routes, controllers and views for all CRUD operations

--- controllers/designerController.js
+++ controllers/designerController.js
@@ -3,6 +3,7 @@ const Boardgame = require('../models/boardgame');
 
 const async = require('async');
 const { body, validationResult } = require('express-validator');
+const designer = require('../models/designer');
 
 exports.designer_list = function (req, res, next) {
 	Designer.find()
@@ -67,11 +68,112 @@ exports.designer_create_post = [
 			// There are errors. Render form again with sanitized values/errors messages.
 			res.render('designer_form', {
 				title: 'Create Designer',
-				author: req.body,
+				designer: req.body,
 				errors: errors.array(),
 			});
 			return;
 		}
-		res.render('designer_form', { title: 'Create Designer' });
+		// Data from form is valid.
+
+		// Create a Designer object with escaped and trimmed data.
+		const designer = new Designer({
+			name: req.body.name,
+		});
+		designer.save((err) => {
+			if (err) {
+				return next(err);
+			}
+			// Successful - redirect to new designer record.
+			res.redirect(designer.url);
+		});
+	},
+];
+
+exports.designer_delete_get = function (req, res, next) {
+	async.parallel(
+		{
+			designer(callback) {
+				Designer.findById(req.params.id).exec(callback);
+			},
+			designers_boardgames(callback) {
+				Boardgame.find({ designers: req.params.id }).exec(callback);
+			},
+		},
+		(err, results) => {
+			if (err) {
+				return next(err);
+			}
+			if (results.designer == null) {
+				res.redirect('/designer');
+			}
+			res.render('designer_delete', {
+				title: 'Delete Designer',
+				designer: results.designer,
+				designer_boardgames: results.designers_boardgames,
+			});
+		}
+	);
+};
+
+exports.designer_delete_post = function (req, res, next) {
+	Designer.findByIdAndDelete(req.params.id, (err) => {
+		if (err) {
+			return next(err);
+		}
+		res.redirect('/designer');
+	});
+};
+
+exports.designer_update_get = function (req, res, next) {
+	Designer.findById(req.params.id).exec(function (err, designer) {
+		if (err) {
+			return next(err);
+		}
+		res.render('designer_form', {
+			title: 'Update Designer',
+			designer: designer,
+		});
+	});
+};
+
+exports.designer_update_post = [
+	body('name')
+		.trim()
+		.isLength({ min: 1 })
+		.escape()
+		.withMessage('You must enter a name.'),
+	(req, res, next) => {
+		// Extract the validation errors from a request.
+		const errors = validationResult(req);
+
+		// Create a Designer object with escaped and trimmed data.
+		const designer = new Designer({
+			name: req.body.name,
+			_id: req.params.id,
+		});
+
+		if (!errors.isEmpty()) {
+			// There are errors. Render form again with sanitized values/errors messages.
+			res.render('designer_form', {
+				title: 'Update Designer',
+				designer,
+				errors: errors.array(),
+			});
+			return;
+		}
+		// Data from form is valid.
+
+		Designer.findByIdAndUpdate(
+			req.params.id,
+			designer,
+			(err, thedesigner) => {
+				if (err) {
+					return next(err);
+				}
+
+				// Successful: redirect to designer detail page.
+				res.redirect(thedesigner.url);
+			}
+		);
 	},
 ];
--- routes/designer.js
+++ routes/designer.js
@@ -9,6 +9,14 @@ router.get('/create', designer_controller.designer_create_get);
 
 router.post('/create', designer_controller.designer_create_post);
 
+router.get('/:id/delete', designer_controller.designer_delete_get);
+
+router.post('/:id/delete', designer_controller.designer_delete_post);
+
+router.get('/:id/update', designer_controller.designer_update_get);
+
+router.post('/:id/update', designer_controller.designer_update_post);
+
 router.get('/:id', designer_controller.designer_detail);
 
 module.exports = router;
